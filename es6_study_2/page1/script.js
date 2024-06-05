4ae1f1d05198c7ba58ad21a2eadcfecbe10a1269
YosaAE24 <cebastin2424@gmail.com>
2024-06-05 14:17:58 +0700

    mempelajari cara menggunakan 'iterasi' untuk mengulangi sebagian code secara efisien.

---
+++ es6_study_2/page1/script.js
@@ -0,0 +1,17 @@
+// Deklarasikan variable number
+let number = 1;
+
+// Cetak nilai dari variable number, dan tambahkan dengan 1
+console.log(number);
+number += 1;
+console.log(number);
+
+// Salin kedua baris di atas dan tempelkan di baris bawah sebanyak 4 kali
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
+number += 1;
+console.log(number);
