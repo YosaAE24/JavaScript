32375ace02a3e75ae257b9ac2c637bc5975cc0a4
YosaAE24 <cebastin2424@gmail.com>
2024-06-05 14:21:19 +0700

    mempelajari method untuk menangani array yang penting bagi pengembangan JavaScript yang praktis.

---
+++ es6_study_6/page2/script.js
@@ -0,0 +1,6 @@
+const characters = ["Ninja Ken", "Baby Ben", "Guru Domba", "Birdie"];
+
+// Cetak semua element di dalam array characters dengan menggunakan method forEach
+characters.forEach((character) => {
+  console.log(character)
+});
